d7c648cef57407d284123ccaa79d92977408d694
rahad404 <rahad.0911@gmail.com>
2024-09-04 23:28:15 +0600

    q1 custom msg func

---
+++ Summer24_SPL/7. Function_problems/fn1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void print(); //function prototype
+
+// main function 
+int main(){
+    print();
+}
+
+// print function 
+void print(){
+    printf("\nThis is a function\n");
+}
