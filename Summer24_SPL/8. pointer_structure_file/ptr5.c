2c14728e1b129dea543513882bff13a4f474b455
rahad404 <rahad.0911@gmail.com>
2024-10-02 11:40:07 +0600

    edit ptr5.c || (pointer) swap two value

--- Summer24_SPL/8. pointer_structure_file/ptr5.c
+++ Summer24_SPL/8. pointer_structure_file/ptr5.c
@@ -1,11 +1,20 @@
 #include<stdio.h>
-
+/*
 void swap(int *a,int *b){
     int *temp;
     temp = a;
     a = b;
     b = temp;
-    printf("befor swap: a=%d b=%d\n",*a,*b);
+    printf("befor swap(function): a=%d b=%d\n",*a,*b);
+}
+*/
+
+void swap(int *a,int *b){
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+    printf("befor swap(function): a=%d b=%d\n",*a,*b);
 
 }
 
@@ -16,7 +25,9 @@ int main(){
     printf("enter two number: ");
     scanf("%d%d",ptra,ptrb);
 
-    printf("befor swap: a=%d b=%d\n",*ptra,*ptrb);
+    printf("befor swap(main): a=%d b=%d\n",*ptra,*ptrb);
 
     swap(ptra,ptrb);
+
+    printf("after swap(main): a=%d b=%d\n",*ptra,*ptrb);
 }
